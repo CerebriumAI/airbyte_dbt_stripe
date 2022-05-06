c7256688b53fc2e875847d003fba4206b4a6a672
Elijah Roussos <elijah.rou@gmail.com>
2022-05-06 03:33:35 +0000

    add new fields to subscriptions stg table

--- models/tmp/stg_stripe__subscriptions.sql
+++ models/tmp/stg_stripe__subscriptions.sql
@@ -1,6 +1,15 @@
 select
     id as subscription_id,
-    billing as subscription_billing,
+    customer as customer_id,
+    status,
     {{ dbt_date.from_unixtimestamp('start') }} as start_date,
-    {{ dbt_date.from_unixtimestamp('ended_at') }} as ended_at
+    {{ dbt_date.from_unixtimestamp('ended_at') }} as ended_at,
+    billing as subscription_billing,
+    {{ dbt_date.from_unixtimestamp('billing_cycle_anchor') }} as billing_cycle_anchor,
+    {{ dbt_date.from_unixtimestamp('canceled_at') }} as canceled_at,
+    {{ dbt_date.from_unixtimestamp('created') }} as created_at,
+    {{ dbt_date.from_unixtimestamp('current_period_start') }} as current_period_start,
+    {{ dbt_date.from_unixtimestamp('current_period_end') }} as current_period_end,
+    days_until_due,
+    cancel_at_period_end as is_cancel_at_period_end
 from {{ var('subscriptions') }}
